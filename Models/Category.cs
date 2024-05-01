3bdc16cd2ec4703ee607da72679c374366eb0091
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-02 01:15:55 +0300

    Add Id and Name properties to Category model

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,7 +1,12 @@
-﻿namespace e_commerce_website.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace e_commerce_website.Models
 {
     public class Category
     {
-
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
     }
 }
