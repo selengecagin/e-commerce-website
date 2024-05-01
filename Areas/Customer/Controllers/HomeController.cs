77f5b45a0a42213fd137130853bd6212902e3f03
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-02 00:55:01 +0300

    Assdign Customer area to controller

--- Areas/Customer/Controllers/HomeController.cs
+++ Areas/Customer/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 
 namespace e_commerce_website.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
