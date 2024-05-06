022e2fb2a3feee332b79de6a95d528ecbd660665
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-06 11:58:19 +0300

    Add IWebHostEnvironment dependency for environment-specific functionality

--- Areas/Admin/Controllers/ProductController.cs
+++ Areas/Admin/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -14,10 +15,12 @@ namespace e_commerce_website.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _he;
 
-        public ProductController(ApplicationDbContext context)
+        public ProductController(ApplicationDbContext context,IWebHostEnvironment he)
         {
             _context = context;
+            _he = he;
         }
 
         // GET: Admin/Product
