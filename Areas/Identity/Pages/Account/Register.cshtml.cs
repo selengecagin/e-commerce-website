6771832a4304947e71ecd09e20fbcc4c2188b461
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-07 21:38:12 +0300

    Check availability of User role in the RoleManager

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -181,17 +181,24 @@ namespace e_commerce_website.Areas.Identity.Pages.Account
                         await _roleManager.CreateAsync(new IdentityRole(Other.Role_Admin))
                     }
 
-                  //  var userId = await _userManager.GetUserIdAsync(user);
-                  //  var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                  //  code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                  //  var callbackUrl = Url.Page(
-                  //      "/Account/ConfirmEmail",
-                  //      pageHandler: null,
-                  //      values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                  //      protocol: Request.Scheme);
 
-                  //  await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                  //      $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    if (!await _roleManager.RoleExistsAsync(Other.Role_User))
+                    {
+
+                        await _roleManager.CreateAsync(new IdentityRole(Other.Role_User))
+                    }
+
+                    //  var userId = await _userManager.GetUserIdAsync(user);
+                    //  var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    //  code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    //  var callbackUrl = Url.Page(
+                    //      "/Account/ConfirmEmail",
+                    //      pageHandler: null,
+                    //      values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                    //      protocol: Request.Scheme);
+
+                    //  await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    //      $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
