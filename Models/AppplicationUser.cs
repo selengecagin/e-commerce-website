b8c35627c0db63612f3aec7762225bba5b129c90
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-07 15:36:58 +0300

    Create ApplicationUser model

---
+++ Models/AppplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace e_commerce_website.Models
+{
+    public class AppplicationUser:IdentityUser
+    {
+        public int MyProperty { get; set; }
+
+
+    }
+}
