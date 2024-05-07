eb9c7c10840c5622d808436782d9f50b6aab9ff6
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-07 15:44:56 +0300

    Define properties for user details and role management in ApplicationUser

--- Models/AppplicationUser.cs
+++ Models/AppplicationUser.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace e_commerce_website.Models
 {
     public class AppplicationUser:IdentityUser
     {
-        public int MyProperty { get; set; }
+        [Required]
+        public string Name { get; set; }
 
+        [Required]
+        public string Surname { get; set; }
 
+        public string Adress { get; set; }
+
+        public string Region { get; set; }
+
+        public string City { get; set; }
+
+        public string Province { get; set; }
+
+        public string ZipCode { get; set; }
+
+        [NotMapped]
+        public string Role { get; set; }
     }
 }
