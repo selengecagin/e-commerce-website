b1e4f0c38c06ea705143cde7b95e015d3ba166a6
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-07 20:55:44 +0300

    Add constants for user roles in the Other class

---
+++ Models/Other.cs
@@ -0,0 +1,10 @@
+﻿namespace e_commerce_website.Models
+{
+    public static class Other
+    {
+        public const string Role_Individual = "Individual";
+        public const string Role_Admin = "Admin";
+        public const string Role_User = "User";
+
+    }
+}
