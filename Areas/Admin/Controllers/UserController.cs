46c59164f8c8ea74b21c5457f32e992374cffec6
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-08 10:25:47 +0300

    Enhance Index action by adding  users, roles, and user roles

--- Areas/Admin/Controllers/UserController.cs
+++ Areas/Admin/Controllers/UserController.cs
@@ -16,6 +16,8 @@ namespace e_commerce_website.Areas.Admin.Controllers
         public IActionResult Index()
         {
             var users= _context.ApplicationUsers.ToList();
+            var role=_context.Roles.ToList();
+            var userRole =_context.UserRoles.ToList();
             return View();
         }
     }
