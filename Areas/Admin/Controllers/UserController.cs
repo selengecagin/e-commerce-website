a164b2acac90c1c9466131cb03a6be46f4868422
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-08 10:22:31 +0300

    Add ApplicationDbContext dependency injection to UserController

---
+++ Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using e_commerce_website.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace e_commerce_website.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
