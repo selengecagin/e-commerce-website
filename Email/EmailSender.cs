d3b92aba02a6a34f8f46b927db928407c4122ece
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-10 11:18:34 +0300

    Implemented SendGrid email functionality using EmailSender class, with dependency injection of SendGrid options

--- Email/EmailSender.cs
+++ Email/EmailSender.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Linq.Expressions;
 
 namespace e_commerce_website.Email
 {
@@ -7,7 +10,17 @@ namespace e_commerce_website.Email
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            throw new NotImplementedException();
+            var client = new SendGridClient(Options.SendGridKey);
+            var message = new SendGridMessage()
+            {
+                From = new EmailAddress("selengecagin.pr@gmail.com", "Selenge"),
+                Subject = subject,
+                PlainTextContent = htmlMessage,
+                HtmlContent = htmlMessage,
+            };
+            message.AddTo(new EmailAddress(email));
+          throw;
+            }
         }
         public EmailOptions Options { get; set; }
         public EmailSender(IOptions <EmailOptions> emailOptions) {
