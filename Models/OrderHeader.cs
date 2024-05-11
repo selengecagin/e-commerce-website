1a555d5daa524fdec7c571ea712a4411ac8da28f
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-12 00:29:32 +0300

    Implement OrderHeader model for managing order information

---
+++ Models/OrderHeader.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace e_commerce_website.Models
+{
+    public class OrderHeader
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+
+        public string ApplicationUserId { get; set; }
+
+        [ForeignKey("ApplicationUserId")]
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public double OrderTotal { get; set; }
+
+        public string OrderStatus { get; set; }
+    }
+}
