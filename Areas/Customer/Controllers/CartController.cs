fbef0a82a4ee86ccf1ff6f56fdcc4b4bc2ee062f
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-12 12:57:05 +0300

    Add CartController in Customer area

---
+++ Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace e_commerce_website.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
