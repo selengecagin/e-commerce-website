2738bbdfdbd726c89a84b6938add03e761215763
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-12 13:00:00 +0300

    Declare ApplicationDbContext, IEmailSender, and UserManager dependencies

--- Areas/Customer/Controllers/CartController.cs
+++ Areas/Customer/Controllers/CartController.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using e_commerce_website.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace e_commerce_website.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class CartController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        private readonly IEmailSender _emailSender;
+        private readonly UserManager<IdentityUser> _userManager;
+
+
         public IActionResult Index()
         {
             return View();
