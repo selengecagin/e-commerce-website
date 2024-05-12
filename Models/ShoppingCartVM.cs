1d053497c90504c5d1a144f590d0f224237873da
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-12 10:05:12 +0300

    Create ShoppingCartVM with ListCart and OrderHeader properties

---
+++ Models/ShoppingCartVM.cs
@@ -0,0 +1,10 @@
+﻿namespace e_commerce_website.Models
+{
+    public class ShoppingCartVM
+    {
+        public IEnumerable<ShoppingCart> ListCart { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+
+
+    }
+}
