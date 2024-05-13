bc08b338d25ad4f629ca7883ab0fb983cd1c8fed
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-13 19:28:16 +0300

    Fix URL generation for confirmation email

--- Areas/Customer/Controllers/CartController.cs
+++ Areas/Customer/Controllers/CartController.cs
@@ -72,11 +72,12 @@ namespace e_commerce_website.Areas.Customer.Controllers
             var callbackUrl = Url.Page(
                "/Account/ConfirmEmail",
                pageHandler: null,
-                values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                values: new { area = "Identity", userId = user.Id, code = code,},
               protocol: Request.Scheme);
 
-            await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+            await _emailSender.SendEmailAsync(user.Email, "Confirm your email",
             $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+          
         }
 
     }
