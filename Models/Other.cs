722269ecc5d697046f31929b708ee30d815312bc
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-20 17:08:10 +0300

    Define order statuses: Confirmed, OnHold, Shipped

--- Models/Other.cs
+++ Models/Other.cs
@@ -6,5 +6,9 @@
         public const string Role_Admin = "Admin";
         public const string Role_User = "User";
         public const string ssShoppingCart  = "Shopping Cart Session";
+        public const string OrderConfirmed = "Confirmed";
+        public const string OrderOnHold = "OnHold";
+        public const string OrderShipped = "Shipped";
+
     }
 }
