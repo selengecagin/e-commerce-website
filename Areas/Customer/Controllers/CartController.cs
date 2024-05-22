b35ea9906082c88b02bbf78dccaee0620955fc42
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-22 18:17:35 +0300

    CartController Error

--- Areas/Customer/Controllers/CartController.cs
+++ Areas/Customer/Controllers/CartController.cs
@@ -110,7 +110,7 @@ namespace e_commerce_website.Areas.Customer.Controllers
 
             CreatePaymentRequest request = new CreatePaymentRequest();
             request.Locale = Locale.TR.ToString();
-            request.ConversationId = new Random().Next(1111,9999).ToString();
+            request.ConversationId = new Random().Next(1111, 9999).ToString();
             request.Price = model.OrderHeader.OrderTotal.ToString();
             request.PaidPrice = model.OrderHeader.OrderTotal.ToString();
             request.Currency = Currency.TRY.ToString();
@@ -119,7 +119,7 @@ namespace e_commerce_website.Areas.Customer.Controllers
             request.PaymentChannel = PaymentChannel.WEB.ToString();
             request.PaymentGroup = PaymentGroup.PRODUCT.ToString();
 
-           {/* PaymentCard paymentCard = new PaymentCard();
+            {/* PaymentCard paymentCard = new PaymentCard();
             paymentCard.CardHolderName = "John Doe";
             paymentCard.CardNumber = "5528790000000008";
             paymentCard.ExpireMonth = "12";
@@ -129,169 +129,170 @@ namespace e_commerce_website.Areas.Customer.Controllers
             request.PaymentCard = paymentCard;
             */
 
-            PaymentCard paymentCard = new PaymentCard();
-            paymentCard.CardHolderName = model.OrderHeader.CardName;
-            paymentCard.CardNumber = model.OrderHeader.CardNumber;
-            paymentCard.ExpireMonth = model.OrderHeader.ExpirationMonth;
-            paymentCard.ExpireYear = model.OrderHeader.ExpirationYear;
-            paymentCard.Cvc = model.OrderHeader.CVC;
-            paymentCard.RegisterCard = 0;
-            request.PaymentCard = paymentCard;
+                PaymentCard paymentCard = new PaymentCard();
+                paymentCard.CardHolderName = model.OrderHeader.CardName;
+                paymentCard.CardNumber = model.OrderHeader.CardNumber;
+                paymentCard.ExpireMonth = model.OrderHeader.ExpirationMonth;
+                paymentCard.ExpireYear = model.OrderHeader.ExpirationYear;
+                paymentCard.Cvc = model.OrderHeader.CVC;
+                paymentCard.RegisterCard = 0;
+                request.PaymentCard = paymentCard;
 
-            Buyer buyer = new Buyer();
-            buyer.Id = model.OrderHeader.Id.ToString();
-            buyer.Name = model.OrderHeader.Name;
-            buyer.Surname = model.OrderHeader.Surname;
-            buyer.GsmNumber = model.OrderHeader.PhoneNumber;
-            buyer.Email = "email@email.com";
-            buyer.IdentityNumber = "74300864791";
-            buyer.LastLoginDate = "2015-10-05 12:43:35";
-            buyer.RegistrationDate = "2013-04-21 15:12:09";
-            buyer.RegistrationAddress = model.OrderHeader.Adress;
-            buyer.Ip = "85.34.78.112";
-            buyer.City = model.OrderHeader.City;
-            buyer.Country = "Turkey";
-            buyer.ZipCode = model.OrderHeader.ZipCode;
-            request.Buyer = buyer;
+                Buyer buyer = new Buyer();
+                buyer.Id = model.OrderHeader.Id.ToString();
+                buyer.Name = model.OrderHeader.Name;
+                buyer.Surname = model.OrderHeader.Surname;
+                buyer.GsmNumber = model.OrderHeader.PhoneNumber;
+                buyer.Email = "email@email.com";
+                buyer.IdentityNumber = "74300864791";
+                buyer.LastLoginDate = "2015-10-05 12:43:35";
+                buyer.RegistrationDate = "2013-04-21 15:12:09";
+                buyer.RegistrationAddress = model.OrderHeader.Adress;
+                buyer.Ip = "85.34.78.112";
+                buyer.City = model.OrderHeader.City;
+                buyer.Country = "Turkey";
+                buyer.ZipCode = model.OrderHeader.ZipCode;
+                request.Buyer = buyer;
 
-            Address shippingAddress = new Address();
-            shippingAddress.ContactName = "Jane Doe";
-            shippingAddress.City = "Istanbul";
-            shippingAddress.Country = "Turkey";
-            shippingAddress.Description = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
-            shippingAddress.ZipCode = "34742";
-            request.ShippingAddress = shippingAddress;
+                Address shippingAddress = new Address();
+                shippingAddress.ContactName = "Jane Doe";
+                shippingAddress.City = "Istanbul";
+                shippingAddress.Country = "Turkey";
+                shippingAddress.Description = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
+                shippingAddress.ZipCode = "34742";
+                request.ShippingAddress = shippingAddress;
 
-            Address billingAddress = new Address();
-            billingAddress.ContactName = "Jane Doe";
-            billingAddress.City = "Istanbul";
-            billingAddress.Country = "Turkey";
-            billingAddress.Description = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
-            billingAddress.ZipCode = "34742";
-            request.BillingAddress = billingAddress;
+                Address billingAddress = new Address();
+                billingAddress.ContactName = "Jane Doe";
+                billingAddress.City = "Istanbul";
+                billingAddress.Country = "Turkey";
+                billingAddress.Description = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
+                billingAddress.ZipCode = "34742";
+                request.BillingAddress = billingAddress;
 
-            List<BasketItem> basketItems = new List<BasketItem>();
+                List<BasketItem> basketItems = new List<BasketItem>();
                 var claimsIdentity = (ClaimsIdentity)User.Identity;
                 var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
 
                 //bring items in the user basket
-                foreach (var item in _db.ShoppingCart.Where(i=>i.ApplicationUserId == claim.Value).Include(i=>i.Product))
+                foreach (var item in _db.ShoppingCart.Where(i => i.ApplicationUserId == claim.Value).Include(i => i.Product))
                 {
                     basketItems.Add(new BasketItem()
                     {
                         Id = item.Id.ToString(),
-                        Name=item.Product.Title,
-                        Category1=item.Product.CategoryId.ToString(),
+                        Name = item.Product.Title,
+                        Category1 = item.Product.CategoryId.ToString(),
                         ItemType = BasketItemType.PHYSICAL.ToString(),
-                        Price=(item.Price*item.Count).ToString(),  
+                        Price = (item.Price * item.Count).ToString(),
                     });
                 }
                 request.BasketItems = basketItems;
 
-            return Payment.Create(request, options);
-        }
-
-        public IActionResult OrderCompleted()
-        {
-            return View();
-        }
-
-
-        public IActionResult Index()
-        {
-            var claimsIdentity = (ClaimsIdentity)User.Identity;
-            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
-            ShoppingCartVM = new ShoppingCartVM()
-            {
-                OrderHeader = new Models.OrderHeader(),
-                ListCart = _db.ShoppingCart.Where(i=> i.ApplicationUserId == claim.Value).Include(i=>i.Product)
-            };
-
-            ShoppingCartVM.OrderHeader.OrderTotal = 0;
-            ShoppingCartVM.OrderHeader.ApplicationUser = _db.ApplicationUsers.FirstOrDefault(i => i.Id == claim.Value);
-
-            foreach ( var item in ShoppingCartVM.ListCart)
-            {
-                ShoppingCartVM.OrderHeader.OrderTotal += (item.Count * item.Product.Price);
+                return Payment.Create(request, options);
             }
 
-            return View(ShoppingCartVM);
-        }
-
-        [HttpPost]
-        [ActionName("Index")]
-        public async Task<IActionResult> IndexPOST()
-        {
-            var claimsIdentity = (ClaimsIdentity)User.Identity;
-            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
-
-            var user = _db.ApplicationUsers.FirstOrDefault(i => i.Id == claim.Value);
-            if (user == null)
+            public IActionResult OrderCompleted()
             {
-                ModelState.AddModelError(string.Empty, " Failed to confirm email");
+                return View();
             }
 
 
-
-            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-            var callbackUrl = Url.Page(
-               "/Account/ConfirmEmail",
-               pageHandler: null,
-                values: new { area = "Identity", userId = user.Id, code = code,},
-              protocol: Request.Scheme);
-
-            await _emailSender.SendEmailAsync(user.Email, "Confirm your email",
-            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-            ModelState.AddModelError(string.Empty, "Send confirmation code...");
-            return RedirectToAction("Success");
-        }
-        public IActionResult Success()
-        {
-            return View();
-        }
-
-        public IActionResult Add(int cartId)
-        {
-            var cart = _db.ShoppingCart.FirstOrDefault(i => i.Id == cartId);
-            cart.Count += 1;
-            _db.SaveChanges();
-            return RedirectToAction(nameof(Index));
-        }
-
-        public IActionResult Decrease(int cartId)
-        {
-            var cart = _db.ShoppingCart.FirstOrDefault(i => i.Id == cartId);
-
-            if (cart.Count==1)
+            public IActionResult Index()
             {
-                var count = _db.ShoppingCart.Where(i => i.ApplicationUserId == cart.ApplicationUserId).ToList().Count();
-                _db.ShoppingCart.Remove(cart);
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                ShoppingCartVM = new ShoppingCartVM()
+                {
+                    OrderHeader = new Models.OrderHeader(),
+                    ListCart = _db.ShoppingCart.Where(i => i.ApplicationUserId == claim.Value).Include(i => i.Product)
+                };
+
+                ShoppingCartVM.OrderHeader.OrderTotal = 0;
+                ShoppingCartVM.OrderHeader.ApplicationUser = _db.ApplicationUsers.FirstOrDefault(i => i.Id == claim.Value);
+
+                foreach (var item in ShoppingCartVM.ListCart)
+                {
+                    ShoppingCartVM.OrderHeader.OrderTotal += (item.Count * item.Product.Price);
+                }
+
+                return View(ShoppingCartVM);
+            }
+
+            [HttpPost]
+            [ActionName("Index")]
+            public async Task<IActionResult> IndexPOST()
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+                var user = _db.ApplicationUsers.FirstOrDefault(i => i.Id == claim.Value);
+                if (user == null)
+                {
+                    ModelState.AddModelError(string.Empty, " Failed to confirm email");
+                }
+
+
+
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                var callbackUrl = Url.Page(
+                   "/Account/ConfirmEmail",
+                   pageHandler: null,
+                    values: new { area = "Identity", userId = user.Id, code = code, },
+                  protocol: Request.Scheme);
+
+                await _emailSender.SendEmailAsync(user.Email, "Confirm your email",
+                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                ModelState.AddModelError(string.Empty, "Send confirmation code...");
+                return RedirectToAction("Success");
+            }
+
+            public IActionResult Success()
+            {
+                return View();
+            }
+
+            public IActionResult Add(int cartId)
+            {
+                var cart = _db.ShoppingCart.FirstOrDefault(i => i.Id == cartId);
+                cart.Count += 1;
                 _db.SaveChanges();
-                HttpContext.Session.SetInt32(Other.ssShoppingCart,count-1);
+                return RedirectToAction(nameof(Index));
             }
-            else
+
+            public IActionResult Decrease(int cartId)
             {
-                cart.Count -= 1;
-                _db.SaveChanges();
+                var cart = _db.ShoppingCart.FirstOrDefault(i => i.Id == cartId);
+
+                if (cart.Count == 1)
+                {
+                    var count = _db.ShoppingCart.Where(i => i.ApplicationUserId == cart.ApplicationUserId).ToList().Count();
+                    _db.ShoppingCart.Remove(cart);
+                    _db.SaveChanges();
+                    HttpContext.Session.SetInt32(Other.ssShoppingCart, count - 1);
+                }
+                else
+                {
+                    cart.Count -= 1;
+                    _db.SaveChanges();
+                }
+
+                return RedirectToAction(nameof(Index));
             }
 
-            return RedirectToAction(nameof(Index));
-        }
+            public IActionResult Remove(int cartId)
+            {
+                var cart = _db.ShoppingCart.FirstOrDefault(i => i.Id == cartId);
 
-        public IActionResult Remove(int cartId)
-        {
-            var cart = _db.ShoppingCart.FirstOrDefault(i => i.Id == cartId);
-          
                 var count = _db.ShoppingCart.Where(i => i.ApplicationUserId == cart.ApplicationUserId).ToList().Count();
                 _db.ShoppingCart.Remove(cart);
                 _db.SaveChanges();
                 HttpContext.Session.SetInt32(Other.ssShoppingCart, count - 1);
-            
-            return RedirectToAction(nameof(Index));
+
+                return RedirectToAction(nameof(Index));
+            }
+
         }
 
-    }
-
-}
+    } }
