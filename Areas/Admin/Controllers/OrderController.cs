048e156b0e917650f711363576f7617093cd67e3
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-24 12:43:41 +0300

    Add ApplicationDbContext field to OrderController

--- Areas/Admin/Controllers/OrderController.cs
+++ Areas/Admin/Controllers/OrderController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using e_commerce_website.Areas.Admin.Controllers;
+using e_commerce_website.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace e_commerce_website.Areas.Admin.Controllers
@@ -7,9 +9,19 @@ namespace e_commerce_website.Areas.Admin.Controllers
     [Authorize]
     public class OrderController : Controller
     {
+        private readonly ApplicationDbContext _db;
+  
+
         public IActionResult Index()
         {
             return View();
         }
     }
+}
+
+
+
+public OrderController(ApplicationDbContext db)
+{
+
 }
