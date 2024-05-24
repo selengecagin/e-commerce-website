da26dc852205eeccccc6b5ca999b132ff8f572d9
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-24 13:13:26 +0300

    Filter OrderHeaders based on user role in OrderController Index

--- Areas/Admin/Controllers/OrderController.cs
+++ Areas/Admin/Controllers/OrderController.cs
@@ -1,7 +1,9 @@
 ﻿using e_commerce_website.Areas.Admin.Controllers;
 using e_commerce_website.Data;
+using e_commerce_website.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 
 namespace e_commerce_website.Areas.Admin.Controllers
@@ -20,7 +22,17 @@ namespace e_commerce_website.Areas.Admin.Controllers
         {
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
-            return View();
+            IEnumerable<OrderHeader> orderHeaderList;
+
+            if(User.IsInRole(Other.Role_Admin))
+            {
+                orderHeaderList = _db.OrderHeaders.ToList();
+            }
+            else
+            {
+                orderHeaderList = _db.OrderHeaders.Where(i => i.ApplicationUserId == claim.Value).Include(i => i.ApplicationUser);
+            }
+            return View(orderHeaderList);
         }
     }
 }
