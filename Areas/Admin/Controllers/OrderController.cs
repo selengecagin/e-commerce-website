d178106a037c38039eeb1d776728525b4b7b569f
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-24 12:41:33 +0300

    Add OrderController with Index view and authorization

---
+++ Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace e_commerce_website.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
