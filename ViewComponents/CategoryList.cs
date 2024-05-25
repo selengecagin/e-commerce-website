6ef37cdf59a87461a670abce775364acb5071064
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-05-25 14:51:14 +0300

    Create CategoryList ViewComponent with dependency injection

--- ViewComponents/CategoryList.cs
+++ ViewComponents/CategoryList.cs
@@ -1,8 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using e_commerce_website.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace e_commerce_website.ViewComponents
 {
     public class CategoryList : ViewComponent
     {
+
+        private readonly ApplicationDbContext _db;
+        public CategoryList(ApplicationDbContext db)
+        {
+            _db = db;
+        }
     }
 }
